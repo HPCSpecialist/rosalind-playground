d8253fd085a78367ab49b208dfb5096fdb646e99
Seyed Majid Barekati <4667588+sedbar@users.noreply.github.com>
2026-06-13 12:19:59 +0330

    Add Jupyter notebook and Python script for DNA problem

---
+++ 01_DNA-Counting-DNA-Nucleotides/rosalind_dna.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Rosalind Problem: Counting DNA Nucleotides
+Problem ID: DNA
+Link: https://rosalind.info/problems/dna/
+
+Solution by: SMB (Seyed Majid Barekati)
+GitHub: https://github.com/HPCSpecialist/rosalind-playground
+"""
+
+def count_nucleotides(dna_string):
+    a = dna_string.count('A')
+    c = dna_string.count('C')
+    g = dna_string.count('G')
+    t = dna_string.count('T')
+    return a, c, g, t
+
+def main():
+    with open("rosalind_dna.txt", "r") as file:
+        dna = file.read().strip()
+    a, c, g, t = count_nucleotides(dna)
+    print(f"{a} {c} {g} {t}")
+
+if __name__ == "__main__":
+    main()
